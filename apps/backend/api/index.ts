2cae48f316c69a0eed2350f40f2a628bf6d1bd88
pavelnagaj <pavel.nagai@mail.ru>
2025-05-26 19:54:55 +0300

    fix cors

--- apps/backend/api/index.ts
+++ apps/backend/api/index.ts
@@ -20,21 +20,20 @@ const initialize = async () => {
 app.use(
   '*',
   cors({
-    // origin: (origin) => {
-    //   if (!origin) return '*';
-    //   if (
-    //     origin === 'http://localhost:5173' ||
-    //     origin === 'https://stylish-images.ru' ||
-    //     origin === 'https://ghible.netlify.app' ||
-    //     origin.endsWith('.stylish-images.ru') ||
-    //     origin.includes('.telegram.org') ||
-    //     origin.includes('.t.me')
-    //   ) {
-    //     return origin;
-    //   }
-    //   return null;
-    // },
-    origin: '*',
+    origin: (origin) => {
+      if (!origin) return '*';
+      if (
+        origin === 'http://localhost:5173' ||
+        origin === 'https://stylish-images.ru' ||
+        origin === 'https://ghible.netlify.app' ||
+        origin.endsWith('.stylish-images.ru') ||
+        origin.includes('.telegram.org') ||
+        origin.includes('.t.me')
+      ) {
+        return origin;
+      }
+      return null;
+    },
     allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowHeaders: ['Content-Type', 'Accept', 'Origin'],
     maxAge: 86400,
