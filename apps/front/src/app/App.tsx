3c34c1c066cb6026a71b119b42c0638cf71a792d
pavelnagaj <pavel.nagai@mail.ru>
2025-05-26 19:21:14 +0300

    commit telegram

--- apps/front/src/app/App.tsx
+++ apps/front/src/app/App.tsx
@@ -1,17 +1,18 @@
 import './App.css';
 // import { useMe } from '@/shared/hooks/useMe';
 // import { useTelegram } from '@/shared/hooks/useTelegram';
-import { useTopSafeArea } from '@/shared/hooks/useTopSafeArea';
+// import { useTopSafeArea } from '@/shared/hooks/useTopSafeArea';
 import { BrowserRouter } from 'react-router-dom';
 import Router from './providers/router';
 
 export const App = () => {
   // useMe();
   // useTelegram();
-  const { topSafeAreaOffset } = useTopSafeArea(10);
+  // const { topSafeAreaOffset } = useTopSafeArea(10);
 
   return (
-    <div className="container" style={{ paddingTop: topSafeAreaOffset }}>
+    // <div className="container" style={{ paddingTop: topSafeAreaOffset }}>
+    <div className="container">
       <BrowserRouter>
         <Router />
       </BrowserRouter>
